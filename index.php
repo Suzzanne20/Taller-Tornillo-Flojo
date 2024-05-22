fe7d7f95f3b6bb1fe065979a7440f7e8fa43d566
NOE ESQUIVEL <eesquivelc@miumg.edu.gt>
2024-05-21 19:05:29 -0600

    commit noe

--- index.php
+++ index.php
@@ -8,6 +8,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>TALLER TF</title>
 </head>
-<?php $contenido = ""; include 'layout/plantilla.blade.php';?>
-
+<?php $contenido = ""; include 'indexprincipal.php';?>
 </html>
